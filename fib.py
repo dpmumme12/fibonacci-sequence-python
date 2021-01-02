616320e9c1deab1583f6898e72734620bf1616a9
Doug Mumme <dougmumme@gmail.com>
2021-01-02 17:11:38 -0600

    First commit

---
+++ fib.py
@@ -0,0 +1,15 @@
+from time import sleep
+
+def fib_seq(terms):
+    n1, n2 = 0, 1
+    print("Fibonacci sequence:")
+    for x in range(terms):
+        print(n1)
+        n = n1 + n2
+        n1 = n2
+        n2 = n
+        sleep(1)
+
+terms = int(input("How many sequences? "))
+
+fib_seq(terms)
